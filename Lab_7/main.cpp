a94afc7d5d79a03b012a05c931e9e2e48b93cd90
Семина Анастасия <nasts2@yandex.ru>
2020-06-13 02:01:43 +0300

    Add Lab_7 (without P5)

---
+++ Lab_7/main.cpp
@@ -0,0 +1,205 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int transformation, offset, quantity;
+double sharpen;
+char symbol;
+int number, h, w, max_color;
+vector<unsigned char> image, R_, G_, B_;
+vector<double> R, G, B;
+FILE *fin, *fout;
+
+unsigned char find_nearest(double color)
+{
+    return (unsigned char)min(max(color, 0.), 255.);
+}
+
+double Sat(double c)
+{
+    return min(1.0, max(0.0, c));
+}
+
+double mix(double x, double y, double a)
+{
+    return x * (1 - a) + y * a;
+}
+
+int main(int argc, char *argv[])
+{
+    //lab7.exe <input> <output> <sharpen>
+    if (argc == 4)
+    {
+        try
+        {
+            sharpen = stof(argv[3]);
+            if(sharpen < 0 || sharpen > 1)
+            {
+                cerr << "sharpen < 0 || sharpen > 1";
+                return 1;
+            }
+        }
+        catch(...)
+        {
+            cerr << "Invalid arguments";
+            return 1;
+        }
+    }
+    else
+    {
+        cerr << "Invalid number of arguments";
+        return 1;
+    }
+
+
+    fin = fopen(argv[1], "rb");
+    if (fin == NULL)
+    {
+        cerr << "Input file can't be open";
+        return 1;
+    }
+    quantity = fscanf(fin, "%c%d\n%d%d\n%d\n", &symbol, &number, &w, &h, &max_color);
+    if (quantity != 5 || symbol != 'P' || (number != 5 && number != 6) || h <= 0 || w <= 0 || max_color <= 0 || max_color > 255)
+    {
+        fclose(fin);
+        cerr << "Invalid ppm file";
+        return 1;
+    }
+    if (number == 5)
+    {
+        image.resize(h * w, 0);
+        quantity = fread(&image[0], sizeof(unsigned char), image.size(), fin);
+        fclose(fin);
+        if (quantity != h * w)
+        {
+            cerr << "Invalid pgm file: quantity != h * w";
+            return 1;
+        }
+    }
+    else
+    {
+        image.resize(3 * h * w, 0);
+        R.resize(h * w, 0);
+        G.resize(h * w, 0);
+        B.resize(h * w, 0);
+        R_.resize(h * w, 0);
+        G_.resize(h * w, 0);
+        B_.resize(h * w, 0);
+        quantity = fread(&image[0], sizeof(unsigned char), image.size(), fin);
+        fclose(fin);
+        if (quantity != 3 * h * w)
+        {
+            cerr << "Invalid pnm file: quantity != 3 * h * w";
+            return 1;
+        }
+        for(int i = 0; i < quantity; i += 3)
+        {
+            R[i / 3] = image[i] / 255.;
+            G[i / 3] = image[i + 1] / 255.;
+            B[i / 3] = image[i + 2] / 255.;
+            R_[i / 3] = image[i] ;
+            G_[i / 3] = image[i + 1];
+            B_[i / 3] = image[i + 2];
+        }
+    }
+
+    if (number == 6)
+    {
+        for (int y = 1; y < h - 1; ++y)
+            for (int x = 1; x < w - 1; ++x)
+            {
+                int a = (y - 1) * w + x - 1;
+                int b = (y - 1) * w + x;
+                int c = (y - 1) * w + x + 1;
+                int d = y * w + x - 1;
+                int e = y * w + x;
+                int f = y * w + x + 1;
+                int g = (y + 1) * w + x - 1;
+                int h = (y + 1) * w + x;
+                int i = (y + 1) * w + x + 1;
+
+                double min_R = min(R[d], min(R[e], min(R[f], (min(R[b], R[h])))));
+                double min_G = min(G[d], min(G[e], min(G[f], (min(G[b], G[h])))));
+                double min_B = min(B[d], min(B[e], min(B[f], (min(B[b], B[h])))));
+                double min_R2 = min(min_R, min(R[a], min(R[c], (min(R[g], R[i])))));
+                double min_G2 = min(min_G, min(G[a], min(G[c], (min(G[g], G[i])))));
+                double min_B2 = min(min_B, min(B[a], min(B[c], (min(B[g], B[i])))));
+                min_R += min_R2;
+                min_G += min_G2;
+                min_B += min_B2;
+
+                double max_R = max(R[d], max(R[e], max(R[f], (max(R[b], R[h])))));
+                double max_G = max(G[d], max(G[e], max(G[f], (max(G[b], G[h])))));
+                double max_B = max(B[d], max(B[e], max(B[f], (max(B[b], B[h])))));
+                double max_R2 = max(max_R, max(R[a], max(R[c], (max(R[g], R[i])))));
+                double max_G2 = max(max_G, max(G[a], max(G[c], (max(G[g], G[i])))));
+                double max_B2 = max(max_B, max(B[a], max(B[c], (max(B[g], B[i])))));
+                max_R += max_R2;
+                max_G += max_G2;
+                max_B += max_B2;
+
+                double rcpMR = 1. / max_R;
+                double rcpMG = 1. / max_G;
+                double rcpMB = 1. / max_B;
+
+                double ampR= Sat(min(min_R, 2.0 - max_R) * rcpMR);
+                double ampG= Sat(min(min_G, 2.0 - max_G) * rcpMG);
+                double ampB = Sat(min(min_B, 2.0 - max_B) * rcpMB);
+
+                ampR = sqrt(ampR);
+                ampG = sqrt(ampG);
+                ampB = sqrt(ampB);
+
+                // Filter shape.
+                //  0 w 0
+                //  w 1 w
+                //  0 w 0
+                //double peak=AF1_AU1(const1.x);
+                double sharp = - 1. / (mix(8.0,5.0, Sat(sharpen)));
+                double peak = sharp;
+                double wR = ampR * peak;
+                double wG = ampG * peak;
+                double wB = ampB * peak;
+
+                double rcpWeightR = 1. / (1.0 + 4.0 * wR);
+                double rcpWeightG = 1. / (1.0 + 4.0 * wG);
+                double rcpWeightB = 1. / (1.0 + 4.0 * wB);
+
+//            pixR= Sat((R[b] * wR + R[d] * wR + R[f] * wR + R[h] * wR + R[e]) * rcpWeightR);
+//            pixG= Sat((G[b] * wG + G[d] * wG + G[f] * wG + G[h] * wG + G[e]) * rcpWeightG);
+//            pixB= Sat((B[b] * wB + B[d] * wB + B[f] * wB + B[h] * wB + B[e]) * rcpWeightB);
+                R_[y * w + x] = (unsigned char)(255 * Sat((R[b] * wR + R[d] * wR + R[f] * wR + R[h] * wR + R[e]) * rcpWeightR));
+                G_[y * w + x] = (unsigned char)(255 * Sat((G[b] * wG + G[d] * wG + G[f] * wG + G[h] * wG + G[e]) * rcpWeightG));
+                B_[y * w + x] = (unsigned char)(255 * Sat((B[b] * wB + B[d] * wB + B[f] * wB + B[h] * wB + B[e]) * rcpWeightB));
+            }
+
+        for(int i = 0; i < h * w; i++)
+        {
+            image[3 * i] = R_[i];
+            image[3 * i + 1] = G_[i];
+            image[3 * i + 2] = B_[i];
+        }
+    }
+
+
+    fout = fopen(argv[2], "wb");
+    if (fout == NULL)
+    {
+        cerr << "Output file can't be open";
+        return 1;
+    }
+    fprintf(fout, "%c%d\n%d %d\n%d\n", symbol, number, w, h, max_color);
+    int quantity_out = fwrite(&image[0], sizeof(unsigned char), image.size(), fout);
+    fclose(fout);
+    if (quantity != quantity_out)
+    {
+        cerr << "Quantity of output file != width * height";
+        return 1;
+    }
+    return 0;
+}
